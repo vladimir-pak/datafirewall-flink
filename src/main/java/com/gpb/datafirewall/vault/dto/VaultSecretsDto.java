4ddd958c8c4dbfc8bfdbcc28525e237dcd51c48e
Vladimir Pak <pvolodyas@gmail.com>
2026-05-21 18:17:22 +0300

    fixed VaultSecretsDto

--- src/main/java/com/gpb/datafirewall/vault/dto/VaultSecretsDto.java
+++ src/main/java/com/gpb/datafirewall/vault/dto/VaultSecretsDto.java
@@ -1,11 +1,17 @@
 package com.gpb.datafirewall.vault.dto;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 public record VaultSecretsDto(
         String keystorePassword,
         String truststorePassword,
         String mqUser,
         String mqPassword
-) {
+) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public VaultSecretsDto {
         requireNotBlank(keystorePassword, "keystorePassword");
